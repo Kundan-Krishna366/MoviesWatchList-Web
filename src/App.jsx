77963a1e32ffde3f7246e2e1071e455d87a04478
Kundan-Krishna366 <kundan.krishna366@gmail.com>
2025-06-25 13:32:18 +0530

    Project1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import './css/App.css'
+import Home from "./pages/Home"
+import Favourites from './pages/Favourites'
+import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar/>
+      <main className='main-content'>
+      <Routes>
+        <Route path='/' element = {<Home/>}></Route>
+        <Route path='/favourites' element = {<Favourites/>}></Route>
+      </Routes>
+    </main>
+    </MovieProvider>
+  )
+}
+
+export default App
